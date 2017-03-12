e077ec1f82a02a7172fc2a10ca8e8802434688d3
Ben Kaminski <ben@benkaminski.com>
2017-03-12 19:20:37 -0400

    Updated to Bootstrap Ver 4 Alpha 6, adjusted WP Bootstrap navwalker php to reflect changes in Bootstrap 4.

--- header.php
+++ header.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 	<head>
     	<meta charset="utf-8">
     	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title><?php wp_title(); ?><?php bloginfo('description'); ?></title>
 
         <!-- favicon and branding icons -->
         
@@ -15,28 +14,31 @@
         <?php wp_head(); ?>
     </head>
         <body <?php body_class(); ?>>
-            <nav class="navbar navbar-default navbar-fixed-top">
-                <div class="container">
-                    <div class="navbar-header">
-                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <i class="fa fa-bars fa-fw fa-2x" aria-hidden="true"></i>
-                      </button>
-                      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><i class="fa fa-home" aria-hidden="true"></i></a>
-                    </div>
-                    <div id="navbar" class="navbar-collapse collapse">
-                        <?php wp_nav_menu(array(
-                                'menu'              => 'top_nav',
-                                'theme_location'    => 'top_nav',
-                                'depth'             => 2,
-                                'container'         => 'div',
-                                'container_class'   => 'text-uppercase',
-                                'menu_class'        => 'nav navbar-nav',
-                                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                                'walker'            => new wp_bootstrap_navwalker()
-                                )
-                        ); ?>
-                    </div>
-                </div>
+            <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                    <a class="navbar-brand" href="#">Navbar</a>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <?php wp_nav_menu(array(
+                                    'menu'              => 'top_nav',
+                                    'theme_location'    => 'top_nav',
+                                    'depth'             => 2,
+                                    'container'         => '',
+                                    'container_class'   => '',
+                                    'container_id'      => '',
+                                    'menu_class'        => 'navbar-nav mr-auto',
+                                    'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                                    'walker'            => new wp_bootstrap_navwalker()
+                                    )
+                            ); ?>
+                            <form class="form-inline my-2 my-lg-0">
+                                <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                            </form>
+                        </div>
+                   
             </nav>
+                        
+                   
             
