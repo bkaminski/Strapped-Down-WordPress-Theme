852142fac87012871b244575d0a48280e55d65a6
Ben Kaminski <ben@benkaminski.com>
2017-03-14 15:06:03 -0400

    Edited functions.php to remove version number from dependency links.

--- functions.php
+++ functions.php
@@ -2,21 +2,21 @@
 //Load Scripts
 function enqueue_strapped_down_scripts()
 {
-    wp_enqueue_script('react-js', '//unpkg.com/react@15.3.2/dist/react.min.js', array(), null, null);
-    wp_enqueue_script('react-dom', '//unpkg.com/react-dom@15.3.2/dist/react-dom.min.js', array(), null, null);
-    wp_enqueue_script('babel-core', '//unpkg.com/babel-core@5.8.38/browser.min.js', array(), null, null);
-    wp_enqueue_script('CloudFlare-Tether', '//cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js', array(), null, true);
-    wp_enqueue_script('bootstrap4a6-js-cdn', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js', array('jquery'), null, true);
-    wp_enqueue_script('strapped-down-js', get_template_directory_uri() . '/js/theme_scripts.js', array('jquery'), null, true);
-    wp_enqueue_script('font-awesome-cdn', '//use.fontawesome.com/05b5b9ecc6.js', false, null, true);
+    wp_enqueue_script('react-js', '//unpkg.com/react@15.3.2/dist/react.min.js', array(), null, null, null);
+    wp_enqueue_script('react-dom', '//unpkg.com/react-dom@15.3.2/dist/react-dom.min.js', array(), null, null, null);
+    wp_enqueue_script('babel-core', '//unpkg.com/babel-core@5.8.38/browser.min.js', array(), null, null, null);
+    wp_enqueue_script('CloudFlare-Tether', '//cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js', array(), null, true, null);
+    wp_enqueue_script('bootstrap4a6-js-cdn', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js', array('jquery'), null, true, null);
+    wp_enqueue_script('strapped-down-js', get_template_directory_uri() . '/js/theme_scripts.js', array('jquery'), null, true, null);
+    wp_enqueue_script('font-awesome-cdn', '//use.fontawesome.com/05b5b9ecc6.js', false, null, true, null);
 }
 add_action('wp_enqueue_scripts', 'enqueue_strapped_down_scripts');
 
 //Load CSS
 function enqueue_strapped_down_styles()
 {
-    wp_enqueue_style('bootstrap4a6-css-cdn', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css');
-    wp_enqueue_style('wp-styles', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('bootstrap4a6-css-cdn', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css', array(), null);
+    wp_enqueue_style('wp-styles', get_template_directory_uri() . '/style.css', array(), null);
 }
 add_action('wp_enqueue_scripts', 'enqueue_strapped_down_styles');
 
