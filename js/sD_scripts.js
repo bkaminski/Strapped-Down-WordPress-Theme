a0da9a3dced92aec323eb258b98d85f11dd2b7b1
Ben Kaminski <ben@benkaminski.com>
2017-03-25 21:08:44 -0400

    renamed theme js file

---
+++ js/sD_scripts.js
@@ -0,0 +1,25 @@
+var $ = jQuery.noConflict();
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 150) {
+        $('#back-up').fadeIn(200); // Fade in the arrow
+    } else {
+        $('#back-up').fadeOut(200); // Else fade out the arrow
+    }
+});
+$('#back-up').click(function() { // When arrow is clicked
+    $('body,html').animate({
+        scrollTop: 0 // Scroll to top of body
+    }, 500);
+});
+
+// Add slideDown animation to dropdown
+$('.dropdown').on('show.bs.dropdown', function(e){
+  $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+});
+
+// Add slideUp animation to dropdown
+$('.dropdown').on('hide.bs.dropdown', function(e){
+  $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+});
