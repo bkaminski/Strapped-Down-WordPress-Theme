9f20a119213b2c4a27cd2e851b362f09f23254d5
Ben Kaminski <ben@benkaminski.com>
2017-04-16 16:01:08 -0400

    Introduced JSON API WP Plugin

--- functions.php
+++ functions.php
@@ -26,6 +26,13 @@ require_once('wp_bootstrap4_navwalker.php');
 register_nav_menus(array(
     'top_nav' => __('Top Menu Navigation', 'strapped_down'),
 ));
+//JSON API for POSTS
+function custom_json_api_prepare_post( $post_response, $post, $context ) {
+     $meta = get_post_meta( $post['ID'] );
+     $post_response[‘meta_field_name’] = $meta;
+        return $post_response;
+    }
+add_filter( 'json_prepare_post', 'custom_json_api_prepare_post', 10, 3 );
 
 //Wordpress Fluid Images Bootstrap 4.0.0-alpha.6
 function bootstrap_fluid_images( $html ){
