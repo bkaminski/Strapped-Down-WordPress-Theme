eb3e96fe243377c52eebcf6a2cbac142ee4fdeda
Ben Kaminski <ben@benkaminski.com>
2018-04-04 17:21:46 -0400

    Code cleanup... much needed

--- inc/contactModal.php
+++ inc/contactModal.php
@@ -1,31 +1,31 @@
 <div class="modal fade" id="contactModal" tabindex="-1" role="dialog" aria-labelledby="contactModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="contactModalLabel">Contact Ben:</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <?php echo do_shortcode('[contact-form-7 id="1702" title="Contact Form"]'); ?>
-        <br />
-        <ul class="list-inline">
-          <li class="list-inline-item">... or on social media?</li>
-          <li class="list-inline-item subnav-anchor">
-            <a target="_blank" href="https://twitter.com/thebenkaminski" class="social-icons"><i class="bk-hover fab fa-twitter fa-fw"></i></a>
-          </li>
-          <li class="list-inline-item subnav-anchor">
-            <a target="_blank" href="https://plus.google.com/u/0/+BenKaminski" class="social-icons"><i class="bk-hover fab fa-google-plus-g fa-fw"></i></a>
-          </li>
-          <li class="list-inline-item subnav-anchor">
-            <a target="_blank" href="https://www.linkedin.com/in/kaminskiben/" class="social-icons"><i class="bk-hover fab fa-linkedin-in fa-fw"></i></a>
-          </li>
-        </ul>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="contactModalLabel">Contact Ben:</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<?php echo do_shortcode('[contact-form-7 id="1702" title="Contact Form"]'); ?>
+				<br />
+				<ul class="list-inline">
+					<li class="list-inline-item">... or on social media?</li>
+					<li class="list-inline-item subnav-anchor">
+						<a target="_blank" href="https://twitter.com/thebenkaminski" class="social-icons"><i class="bk-hover fab fa-twitter fa-fw"></i></a>
+					</li>
+					<li class="list-inline-item subnav-anchor">
+						<a target="_blank" href="https://plus.google.com/u/0/+BenKaminski" class="social-icons"><i class="bk-hover fab fa-google-plus-g fa-fw"></i></a>
+					</li>
+					<li class="list-inline-item subnav-anchor">
+						<a target="_blank" href="https://www.linkedin.com/in/kaminskiben/" class="social-icons"><i class="bk-hover fab fa-linkedin-in fa-fw"></i></a>
+					</li>
+				</ul>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
 </div>
--- personal-blog.php
+++ personal-blog.php
@@ -6,43 +6,47 @@
 * @subpackage strapped_down
 */
 get_header(); ?>
-		<div class="container">
-			<div class="row intContent">
-				<div class="col-lg-12">
-				<br />
-					<h1>Ben's Other Blog:</h1>
+<div class="container">
+	<div class="row intContent">
+		<div class="col-lg-12">
+			<br />
+			<h1>Ben's Other Blog:</h1>
+			<br />
+		</div>
+		<?php query_posts('post_type=post&paged='.$paged.'&cat=1&posts_per_page=3'); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<div class="col-lg-12 intContent">
+			<div class="row mb-2">
+				<div class="col-lg-3 text-center mb-2">
+					<a href='<?php the_permalink(); ?>' rel='bookmark' title='<?php the_title(); ?>'>
+						<?php the_post_thumbnail('full', array('class' => 'img-thumbnail')); ?>
+							
+					</a>
 					<br />
-				</div>
-				<?php query_posts('post_type=post&paged='.$paged.'&cat=1&posts_per_page=3'); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				<div class="col-lg-12 intContent">
-					<div class="row mb-2">
-						<div class="col-lg-3 text-center mb-2">
-							<a href='<?php the_permalink(); ?>' rel='bookmark' title='<?php the_title(); ?>'>
-								<?php the_post_thumbnail('full', array('class' => 'img-thumbnail')); ?>
-							</a>
-							<br />
-							<small><meta itemprop="datePublished" content="<?php the_time('Y-m-d') ?>"><?php the_time('m-d-Y') ?>
-							<br /> by <span itemprop="author"><?php the_author() ?></span></small>
-							<br />
-							<a class="badge badge-pill badge-info p-2 mt-2" href="<?php comments_link(); ?>">
-								<i class="far fa-comments fa-fw"></i>  <?php comments_number( '0', '1', '%' ); ?> Comments
-							</a>
-						</div>
-						<div class="col-lg-9">
-							<h2 class="alert alert-dark"><a class="alert-link" role="alert" href='<?php the_permalink(); ?>' rel='bookmark' title='<?php the_title(); ?>'><span itemprop="name"><?php the_title(); ?></span></a></h2>
-							<?php the_excerpt(); ?>
-						</div>
-					</div>
-					<hr class="mt-5" />
-				</div>
-					<?php endwhile; else: ?>
-						<p><?php _e('Whoops, something went horribly wrong. Please seek help immediately.'); ?></p>
-					<?php endif; ?>
-				<div class="col-lg-12">
-					<?php echo wss_pagination(); ?>
+					<small>
+						<meta itemprop="datePublished" content="<?php the_time('Y-m-d') ?>"><?php the_time('m-d-Y') ?>
+						<br /> by <span itemprop="author"><?php the_author() ?></span>
+					</small>
 					<br />
+					<a class="badge badge-pill badge-info p-2 mt-2" href="<?php comments_link(); ?>">
+						<i class="far fa-comments fa-fw"></i>  <?php comments_number( '0', '1', '%' ); ?> Comments
+					</a>
+				</div>
+				<div class="col-lg-9">
+					<h2 class="alert alert-dark">
+						<a class="alert-link" role="alert" href='<?php the_permalink(); ?>' rel='bookmark' title='<?php the_title(); ?>'><span itemprop="name"><?php the_title(); ?></span></a>
+					</h2>
+					<?php the_excerpt(); ?>
 				</div>
 			</div>
+			<hr class="mt-5" />
 		</div>
+			<?php endwhile; else: ?>
+			<p><?php _e('Whoops, something went horribly wrong. Please seek help immediately.'); ?></p>
+			<?php endif; ?>
+		<div class="col-lg-12">
+			<?php echo wss_pagination(); ?>
+			<br />
+		</div>
+	</div>
+</div>
 <?php get_footer(); ?>
-
